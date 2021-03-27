3547a0bfe2ec1deb501bb965f8c8a4f74d2707a8
Garrick6 <2670775238@qq.com>
2021-03-27 13:25:32 +0800

    HW-Week 4 Commit

--- src/main/java/com/LiGuolong/week4/demo/ConfigDemoServlet.java
+++ src/main/java/com/LiGuolong/week4/demo/ConfigDemoServlet.java
@@ -1,10 +1,18 @@
 package com.LiGuolong.week4.demo;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+@WebServlet(
+        urlPatterns = {"/config"},
+        initParams = {
+                @WebInitParam(name = "name",value = "LiGuolong"),
+                @WebInitParam(name = "studentId",value = "2019211001000918"),
+        },loadOnStartup = 1
+)
 public class ConfigDemoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
